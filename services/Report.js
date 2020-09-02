ff3159bd63cbefc21f9e78564ed64295346a3cbf
Naeem_Abbas <naeemabbasa25@gmail.com>
2020-09-02 17:49:33 +0500

    REPORT

--- services/Report.js
+++ services/Report.js
@@ -4,25 +4,35 @@ const { poolPromise } = require('../config/db');
 
 const GetEmployeeReport = async (req, res) => {
 	try {
-		var query = `	SELECT 
+		var query = `
+		SELECT 
 		EmployeeCode,
 		InsuranceId,
 		TaxationId,
 		Cnic,
 		FirstName,
 		LastName,
-		format(DOB,'dd/MM/yyyy') as DOB, 
-		format(HireDate,'dd/MM/yyyy') as HireDate ,
-		Address,
-		Contact,
+		DOB, 
+		HireDate,
+		emp.[Address],
+		emp.Contact,
 		(SELECT  [Name] FROM [myuser].[LookupItems] WHERE Id=Gender)AS Gender,
 		(SELECT  [Name] FROM [myuser].[LookupItems] WHERE Id=MaritalStatus)AS MaritalStatus,
 		(SELECT  [Name] FROM [myuser].[LookupItems] WHERE Id=Country)AS Country,
 		(SELECT  [Name] FROM [myuser].[LookupItems] WHERE Id=Title)AS Title,
-		 Email 
-		 FROM [dbo].[Employees] emp INNER JOIN [dbo].[EmployeeBankAccount] empbank
-		ON 
-		emp.Id = empbank.EmployeeId`;
+		(SELECT SUM(value) FROM [myuser].[EmployeePayRoll] WHERE EmployeeId=emp.Id) AS Salary,
+		 emp.Email,
+		 BankId, 
+		 IBAN,
+		 EffectiveDate,
+		 IsPrimary,
+		 ( SELECT  [Name] FROM [myuser].[LookupItems] WHERE Id=CurrencyCode)AS CurrencyCode,
+		 [BankName],
+		 [dbo].[Company].CompanyName
+		 FROM [dbo].[Employees] emp 
+		 INNER JOIN [dbo].[EmployeeBankAccount] empbank ON emp.Id = empbank.EmployeeId
+		 INNER JOIN [dbo].[Company]  ON [dbo].[Company].Id=emp.CompanyId
+		 INNER JOIN [dbo].[Bank] ON [dbo].[Bank].Id=empbank.[BankId]`;
 		const pool = await poolPromise
 		const result = await pool.request()
 			.query(query, function (err, profileset) {
